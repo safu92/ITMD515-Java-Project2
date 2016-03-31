6598772924c8f476823b3a0da99151ba45f89964
Safdar <safu92@gmail.com>
2016-03-31 01:18:21 -0500

    Final Commit

---
+++ src/main/java/edu/iit/sat/itmd4515/smatches/mp2/App.java
@@ -0,0 +1,13 @@
+package edu.iit.sat.itmd4515.smatches.mp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
